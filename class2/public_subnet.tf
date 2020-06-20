e018640a64e9c41f15730d8997c91fc7cf8db998
vitaliejicol <vitaliejicol89@gmail.com>
2020-06-20 20:33:01 +0000

    update

--- class2/public_subnet.tf
+++ class2/public_subnet.tf
@@ -1,23 +1,23 @@
 resource "aws_subnet" "public_subnet1" {
   vpc_id     = "${aws_vpc.main.id}"
-  cidr_block = "10.0.101.0/24" 
+  cidr_block = "${var.public_cidr_block1}" 
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
+  availability_zone = "${var.region}a"
   }
 
 
   resource "aws_subnet" "public_subnet2" {
   vpc_id     = "${aws_vpc.main.id}"
-  cidr_block = "10.0.102.0/24"
+  cidr_block = "${var.public_cidr_block2}"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1b"
+  availability_zone = "${var.region}b"
 
    
   }
 
   resource "aws_subnet" "public_subnet3" {
   vpc_id     = "${aws_vpc.main.id}"
-  cidr_block = "10.0.103.0/24"
+  cidr_block = "${var.public_cidr_block3}"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1c"
+  availability_zone = "${var.region}c"
 }
--- class2/variable.tf
+++ class2/variable.tf
@@ -1,3 +1,15 @@
-variable "region" { # -- user will be asked to indicate the region. 
-    default = "us-east-1" # -- if you don't specify the region the system will create  the default one. 
-}
+variable "region" {} 
+variable "cidr_block" {}
+variable "public_cidr_block1" {} 
+variable "public_cidr_block2" {}
+variable "public_cidr_block3" {} 
+
+variable "private_cidr_block1" {}
+variable "private_cidr_block2" {}
+variable "private_cidr_block3" {}
+
+
+
+
+# -- user will be asked to indicate the region. 
+    #default = "us-east-1" # -- if you don't specify the region the system will create  the default one. 
